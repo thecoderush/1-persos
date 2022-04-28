14952fa80ac949d2e219592ace702fd732fe6f10
thecoderush <pheanuk@gmail.com>
2022-04-28 15:45:03 +0200

    Add 3.21: Refactoring code with OOP

--- src/Controller/PersonnageController.php
+++ src/Controller/PersonnageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Personnage;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -23,44 +24,10 @@ class PersonnageController extends AbstractController
      */
     public function persos(): Response
     {
-        $j1 = [
-            'pseudo' => 'Marc',
-            'age' => 25,
-            'sexe' => true,
-            'carac' => [
-                'force' => 3,
-                'agi' => 2,
-                'intel' => 3
-            ]
-        ];
-        $j2 = [
-            'pseudo' => 'Milo',
-            'age' => 30,
-            'sexe' => true,
-            'carac' => [
-                'force' => 5,
-                'agi' => 1,
-                'intel' => 2
-            ]
-        ];
-        $j3 = [
-            'pseudo' => 'Tya',
-            'age' => 22,
-            'sexe' => false,
-            'carac' => [
-                'force' => 1,
-                'agi' => 2,
-                'intel' => 5
-            ]
-        ];
-        $players = [
-            'j1' => $j1,
-            'j2' => $j2,
-            'j3' => $j3
-        ];
+        Personnage::creerPersonnages();
         return $this->render('personnage/persos.html.twig', [
             'controller_name' => 'PersonnageController',
-            'players' => $players
+            'players' => Personnage::$personnages
         ]);
     }
 }
