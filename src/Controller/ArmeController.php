0b970469f1aee21d92ff174742cd8b6ae83b0fd1
thecoderush <pheanuk@gmail.com>
2022-04-29 17:51:10 +0200

    Displaying a List of Weapons in the armes Template with hardcoded Data

--- src/Controller/ArmeController.php
+++ src/Controller/ArmeController.php
@@ -15,6 +15,44 @@ class ArmeController extends AbstractController
     {
         return $this->render('arme/index.html.twig', [
             'controller_name' => 'ArmeController',
+            'arme' => 'test'
         ]);
+    }
+    
+    /**
+     * @Route("/armes", name="armes")
+     */
+    public function armes(): Response
+    {
+        $a1 = [
+            'nom' => 'épée',
+            'carac' => [
+                'desc' => 'Une superbe épée tranchante',
+                'degat' => 10
+            ]
+        ];
+        $a2 = [
+            'nom' => 'arc',
+            'carac' => [
+                'desc' => 'Une arme à distance',
+                'degat' => 7
+            ]
+        ];
+        $a3 = [
+            'nom' => 'hache',
+            'carac' => [
+                'desc' => 'Une arme ou un outil',
+                'degat' => 15
+            ]
+        ];
+        $weapons = [
+            'w1' => $a1,
+            'w2' => $a2,
+            'w3' => $a3 
+       ];
+        return $this->render('arme/armes.html.twig', [
+            'controller_name' => 'ArmeController',
+            'weapons' => $weapons 
+         ]);
     }
 }
