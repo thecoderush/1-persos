b090e4f30e498a9e3a394e24db13d9da4389ef12
thecoderush <pheanuk@gmail.com>
2022-05-01 14:24:06 +0200

    Add 2.6: Replacing hardcoded data armes to OOP class Arme

--- src/Controller/ArmeController.php
+++ src/Controller/ArmeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Arme;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -24,35 +25,10 @@ class ArmeController extends AbstractController
      */
     public function armes(): Response
     {
-        $a1 = [
-            'nom' => 'épée',
-            'carac' => [
-                'desc' => 'Une superbe épée tranchante',
-                'degat' => 10
-            ]
-        ];
-        $a2 = [
-            'nom' => 'arc',
-            'carac' => [
-                'desc' => 'Une arme à distance',
-                'degat' => 7
-            ]
-        ];
-        $a3 = [
-            'nom' => 'hache',
-            'carac' => [
-                'desc' => 'Une arme ou un outil',
-                'degat' => 15
-            ]
-        ];
-        $weapons = [
-            'w1' => $a1,
-            'w2' => $a2,
-            'w3' => $a3 
-       ];
+        Arme::creerArmes();
         return $this->render('arme/armes.html.twig', [
             'controller_name' => 'ArmeController',
-            'weapons' => $weapons 
-         ]);
+            'weapons' => Arme::$armes 
+        ]);
     }
 }
---
+++ src/Entity/Arme.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Entity;
+
+class Arme {
+
+    public $nom;
+    public $carac = [];
+
+    public static $armes = [];
+
+    public function __construct($nom, $carac) {
+
+        $this->nom = $nom;
+        $this->carac = $carac;
+        self::$armes[] = $this;
+    }
+
+    public static function creerArmes() {
+        $a1 = new Arme('épée', [
+            'desc' => 'Une superbe épée tranchante',
+            'degat' => 10
+        ]);
+        $a2 = new Arme('arc', [
+            'desc' => 'Une arme à distance',
+            'degat' => 7
+        ]);
+        $a3 = new Arme('hache', [
+            'desc' => 'Une arme ou un outil',
+            'degat' => 15
+        ]);
+    }
+
+}
